f350379be3c5aa52cdcb510539bc9bca87e3dbc4
Xing Mian <xlancer4@gmail.com>
2024-11-09 17:15:30 +0800

    updated relative path

--- sentiment-analysis/utils/get_model.py
+++ sentiment-analysis/utils/get_model.py
@@ -20,11 +20,11 @@ def get_model_class(model_name):
 
 def get_weights_path(model_name):
     weight_mapping = {
-        "rnn": "../best_models/final_model/SentimentRNN-FinalModel-val_accval_acc=0.78.ckpt",
-        "rnn_freeze": "../best_models/final_model/SentimentRNN-Freeze-FinalModel-val_accval_acc=0.62.ckpt",
-        "bilstm": "../best_models/final_model/SentimentBiLSTM-FinalModel-val_accval_acc=0.76-v1.ckpt",
-        "bigru": "../best_models/final_model/SentimentBiGRU-FinalModel-val_accval_acc=0.76-v1.ckpt",
-        "cnn": "../best_models/final_model/cnn-epoch=08-val_acc=0.76.ckpt",
+        "rnn": "best_models/final_model/SentimentRNN-FinalModel-val_accval_acc=0.78.ckpt",
+        "rnn_freeze": "best_models/final_model/SentimentRNN-Freeze-FinalModel-val_accval_acc=0.62.ckpt",
+        "bilstm": "best_models/final_model/SentimentBiLSTM-FinalModel-val_accval_acc=0.76-v1.ckpt",
+        "bigru": "best_models/final_model/SentimentBiGRU-FinalModel-val_accval_acc=0.76-v1.ckpt",
+        "cnn": "best_models/final_model/cnn-epoch=08-val_acc=0.76.ckpt",
     }
     return weight_mapping.get(model_name.lower())
 
@@ -32,11 +32,11 @@ def get_weights_path(model_name):
 def get_model_config(model_name):
 
     config_mapping = {
-        "rnn": "../best_models/final_config/SentimentRNN_final_config.json",
-        "rnn_freeze": "../best_models/final_config/SentimentRNN-Freeze_final_config.json",
-        "bilstm": "../best_models/final_config/SentimentBiLSTM_final_config.json",
-        "bigru": "../final_config/SentimentBiGRU_final_config.json",
-        "cnn": "../best_models/final_config/ImprovedSentimentCNN_final_config.json",
+        "rnn": "best_models/final_config/SentimentRNN_final_config.json",
+        "rnn_freeze": "best_models/final_config/SentimentRNN-Freeze_final_config.json",
+        "bilstm": "best_models/final_config/SentimentBiLSTM_final_config.json",
+        "bigru": "final_config/SentimentBiGRU_final_config.json",
+        "cnn": "best_models/final_config/ImprovedSentimentCNN_final_config.json",
     }
 
     with open(config_mapping.get(model_name.lower()), "r") as f:
